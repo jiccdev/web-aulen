5d72525db3085a39d41dad21568ab907fbb46f6d
Jose Ignacio <ji.ccdev@outlook.com>
2023-02-07 20:43:33 -0300

    feat(component): add lease section

--- src/pages/inicio/index.js
+++ src/pages/inicio/index.js
@@ -1,12 +1,16 @@
 import React from 'react';
 import LayoutSection from '@/components/Section/LayoutSection';
 import Principal from '@/components/Section/Inicio/Principal';
+import Lease from '@/components/Section/Inicio/Lease';
 
 const Inicio = () => {
   return (
     <LayoutSection>
       {/* SECCIÓN PRINCIPAL */}
       <Principal />
+
+      {/* NOS ENCARGAMOS DE TÚ ARRIENDO POR TÍ */}
+      <Lease />
     </LayoutSection>
   );
 };
