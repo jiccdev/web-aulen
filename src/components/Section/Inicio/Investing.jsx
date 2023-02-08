f25ce22568f192d1344a131eda570e756e5bfdb2
Jose Ignacio <ji.ccdev@outlook.com>
2023-02-07 21:31:38 -0300

    feat(component): add new properties section

---
+++ src/components/Section/Inicio/Investing.jsx
@@ -0,0 +1,50 @@
+import React from 'react';
+import Link from 'next/link';
+import Image from 'next/image';
+import HeaderSection from '../../Section/HeaderSection';
+import AuctionProperties from '../../../assets/img/Inicio/Principal/propiedades-en-remate.jpg';
+import styles from '../../../styles/Section/Inicio/Investing.module.css';
+
+/** Bootstrap components */
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+
+const Investing = () => {
+  return (
+    <div className={styles.investingContainer}>
+      <HeaderSection titleSection="¡Invierte en inmuebles con nosotros!" />
+      <Row className={styles.rowPlanForm}>
+        <Col sm={12} lg={12} xl={12} className={styles.investingCol}>
+          <div className={styles.investingColContent}>
+            <h2 className={styles.investingColTitle}>Propiedades en remates</h2>
+            <p>
+              Un modelo de inversión único en el mercado que genera hasta un 15%
+              de rentabilidad por operación.
+            </p>
+
+            <p>
+              Invierte de manera segura, y cuenta con la asesoría de expertos
+              que te guiaran según tus objetivos, desde la elección del inmueble
+              hasta la capitalización de tu inversión.
+            </p>
+            <div className={styles.consultationContainer}>
+              <Link href="/" className={styles.consultation}>
+                Quiero una asesoría
+              </Link>
+            </div>
+          </div>
+        </Col>
+
+        <div className={styles.investingColAbsolute}>
+          <Image
+            src={AuctionProperties}
+            alt="Propieda en remate"
+            className={styles.investingImg}
+          />
+        </div>
+      </Row>
+    </div>
+  );
+};
+
+export default Investing;
--- src/pages/inicio/index.js
+++ src/pages/inicio/index.js
@@ -3,18 +3,26 @@ import LayoutSection from '@/components/Section/LayoutSection';
 import Principal from '@/components/Section/Inicio/Principal';
 import Lease from '@/components/Section/Inicio/Lease';
 import Steps from '@/components/Section/Inicio/Steps';
+import Investing from '@/components/Section/Inicio/Investing';
+import NewProperties from '@/components/Section/Inicio/NewProperties';
 
 const Inicio = () => {
   return (
     <LayoutSection>
-      {/* SECCIÓN PRINCIPAL */}
+      {/* --SECCIÓN PRINCIPAL-- */}
       <Principal />
 
-      {/* NOS ENCARGAMOS DE TÚ ARRIENDO POR TÍ */}
+      {/* --NOS ENCARGAMOS DE TÚ ARRIENDO POR TÍ-- */}
       <Lease />
 
-      {/* VENDEMOS TÚS PROPIEDADES EN CUATRO SIMPLES PASOS */}
+      {/* --VENDEMOS TÚS PROPIEDADES EN CUATRO SIMPLES PASOS-- */}
       <Steps />
+
+      {/* --INVIERTE EN INMUEBLES CON NOSOTROS-- */}
+      <Investing />
+
+      {/* --PROPIEDADES NUEVAS-- */}
+      <NewProperties />
     </LayoutSection>
   );
 };
