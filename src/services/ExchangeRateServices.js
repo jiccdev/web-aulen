98f9a2317e68e8ecc5cdc9252105405f73fe2775
Jose Contreras <ji.ccdev@outlook.com>
2023-02-16 20:21:38 -0300

    feat(env): add env variables in API services

--- src/services/ExchangeRateServices.js
+++ src/services/ExchangeRateServices.js
@@ -3,8 +3,7 @@ import axios from 'axios';
 const ExchangeRateServices = {
   getExchangeRateUF: async () => {
     const response = await axios.get(
-      // `https://api.cmfchile.cl/api-sbifv3/recursos_api/uf?apikey=${process.env.NEXT_PUBLIC_API_URL_EXCHANGE_RATE_UF_API_KEY}&formato=json`
-      `https://api.cmfchile.cl/api-sbifv3/recursos_api/uf?apikey=b3c682f4e4e29811fa1fd8d3781a463b59181fb7&formato=json`
+      `https://api.cmfchile.cl/api-sbifv3/recursos_api/uf?apikey=${process.env.NEXT_PUBLIC_API_URL_EXCHANGE_RATE_UF_API_KEY}&formato=json`
     );
     return response.data;
   },
--- src/services/SelectsServices.js
+++ src/services/SelectsServices.js
@@ -1,20 +1,10 @@
 import api from '../api';
-import axios from 'axios';
 
 const SelectsServices = {
   getSelects: async () => {
-    const response = await axios.get(
-      `http://190.114.255.247:5050/properties/select-filters`
-    );
+    const response = await api.get(`properties/select-filters`);
     return response.data;
   },
 };
-
-// const SelectsServices = {
-//   getSelects: async () => {
-//     const response = await api.get(`properties/select-filters`);
-//     return response.data;
-//   },
-// };
 
 export default SelectsServices;
