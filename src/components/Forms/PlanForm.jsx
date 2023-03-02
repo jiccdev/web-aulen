cc52c96f5c97ae828a6527d16e07efc73f73ff02
Jose Contreras <ji.ccdev@outlook.com>
2023-03-02 11:35:34 -0300

    feat(component): plan form POST integration

--- src/components/Forms/PlanForm.jsx
+++ src/components/Forms/PlanForm.jsx
@@ -1,18 +1,117 @@
-import React from 'react';
+import React, { useState } from 'react';
+import ToastComponent from '../Toastify/ToastComponent';
+import { toast } from 'react-toastify';
 import { icons } from '../../components/Icons';
+import styles from '../../styles/Forms/PlanForm.module.css';
+import stylesModal from '../../styles/Forms/PlanForm.module.css';
 
 /** Bootstrap components */
 import Button from 'react-bootstrap/Button';
 import Form from 'react-bootstrap/Form';
-import styles from '../../styles/Forms/PlanForm.module.css';
-import stylesModal from '../../styles/Forms/PlanForm.module.css';
+import Col from 'react-bootstrap/Col';
+
+/** API services */
+import ContactFormServices from '@/services/ContactFormServices';
 
 const PlanFrom = ({ props }) => {
+  const [serverErrorMsg, setServerErrorMsg] = useState('');
+  const [formData, setFormData] = useState({
+    name: '',
+    email: '',
+    phone: '',
+    action: '',
+    termsAndConditions: false,
+  });
   const { FaUserAlt, BsTelephoneFill, MdOutlineMailOutline, GrClose } = icons;
 
+  const handleName = (ev) =>
+    setFormData({ ...formData, name: ev.target.value });
+
+  const handleEmail = (ev) =>
+    setFormData({ ...formData, email: ev.target.value });
+
+  const handlePhone = (ev) =>
+    setFormData({ ...formData, phone: ev.target.value });
+
+  const handleAction = (ev) =>
+    setFormData({ ...formData, action: ev.target.value });
+
+  const handleTermsAndConditions = (ev) =>
+    setFormData({
+      ...formData,
+      termsAndConditions: !formData.termsAndConditions,
+    });
+
+  console.log(formData);
+
+  const resetForm = () => {
+    setFormData({
+      name: '',
+      email: '',
+      phone: '',
+      action: '',
+      termsAndConditions: false,
+    });
+  };
+
+  const showToastSuccessMsg = (msg) => {
+    toast.success(msg, {
+      position: 'top-center',
+      autoClose: 1000,
+      hideProgressBar: true,
+      closeOnClick: true,
+      pauseOnHover: true,
+      draggable: true,
+      progress: undefined,
+      theme: 'light',
+    });
+  };
+
+  const showToastErrorMsg = (msg) => {
+    toast.error(msg, {
+      position: 'top-center',
+      autoClose: 1000,
+      hideProgressBar: true,
+      closeOnClick: true,
+      pauseOnHover: true,
+      draggable: true,
+      progress: undefined,
+      theme: 'light',
+    });
+  };
+
+  const showToastWarningMsg = (msg) => {
+    toast.warn(msg, {
+      position: 'top-center',
+      autoClose: 1000,
+      hideProgressBar: true,
+      closeOnClick: true,
+      pauseOnHover: true,
+      draggable: true,
+      progress: undefined,
+      theme: 'light',
+    });
+  };
+
   /** On form submit */
-  const onSubmit = (ev) => {
+  const onFormSubmit = async (ev) => {
     ev.preventDefault();
+    try {
+      const response = await ContactFormServices.addContactForm(formData);
+      if (
+        formData.name === '' &&
+        formData.email === '' &&
+        formData.phone === ''
+      ) {
+        showToastErrorMsg('Todos los campos son obligatorios');
+      } else {
+        showToastSuccessMsg(response.message);
+        resetForm();
+      }
+    } catch (error) {
+      setServerErrorMsg(error.response);
+      showToastWarningMsg('Ocurrio un error al enviar el formulario');
+    }
   };
 
   return (
@@ -30,7 +129,7 @@ const PlanFrom = ({ props }) => {
         ''
       )}
 
-      <Form className={styles.form} onSubmit={onSubmit} id="planForm">
+      <Form className={styles.form} onSubmit={onFormSubmit} id="planForm">
         <h3>¡Despreocúpate por tú propiedad de inversión!</h3>
         <p>Completa el formulacio y entérate como</p>
         <Form.Group className={styles.formGroup} controlId="formBasicName">
@@ -42,6 +141,8 @@ const PlanFrom = ({ props }) => {
             className={styles.formControl}
             placeholder="Nombre"
             name="name"
+            value={formData.name}
+            onChange={handleName}
           />
         </Form.Group>
 
@@ -54,6 +155,8 @@ const PlanFrom = ({ props }) => {
             className={styles.formControl}
             placeholder="Teléfono celular"
             name="phone"
+            value={formData.phone}
+            onChange={handlePhone}
           />
         </Form.Group>
 
@@ -66,6 +169,8 @@ const PlanFrom = ({ props }) => {
             className={styles.formControl}
             placeholder="Correo electrónico"
             name="email"
+            value={formData.email}
+            onChange={handleEmail}
           />
         </Form.Group>
 
@@ -75,18 +180,32 @@ const PlanFrom = ({ props }) => {
         >
           <Form.Check
             type="checkbox"
-            label="Al continuar estás aceptando los términos y condiciones y la política de privacidad"
+            label="Al continuar estás aceptando los términos y condiciones y la
+            política de privacidad."
             className={styles.formCheck}
-            name="terms"
+            name="termsAndConditions"
+            checked={formData.termsAndConditions}
+            onChange={handleTermsAndConditions}
           />
         </Form.Group>
 
-        <Form.Group className={styles.formGroup}>
-          <Button type="submit" className={styles.btnSubmit}>
-            Quiero que me contacten
-          </Button>
-        </Form.Group>
+        <Col sm={12} lg={12}>
+          <Form.Group className={styles.formGroup}>
+            <Button
+              type="submit"
+              onClick={() => {
+                setFormData({ ...formData, action: 'vender' });
+              }}
+              className={styles.btnSubmit}
+            >
+              {/* EN DESARROLLO ❌  */}
+              Quiero que me contacten
+            </Button>
+          </Form.Group>
+        </Col>
       </Form>
+
+      <ToastComponent />
     </div>
   );
 };
