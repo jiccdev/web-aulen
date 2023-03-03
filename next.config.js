954221a96b4df1a28048113c274b826448ecfeaf
Jose Contreras <ji.ccdev@outlook.com>
2023-03-03 20:14:04 -0300

    feat(out): configure export

--- next.config.js
+++ next.config.js
@@ -1,14 +1,18 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  rewrites() {
-    return [
-      {
-        source: '/soy-inversionista/unidades-nuevas',
-        destination: '/soy-inversionista/unidades-nuevas#estacionamientos',
-      },
-    ];
+  images: {
+    unoptimized: true,
   },
+
+  // rewrites() {
+  //   return [
+  //     {
+  //       source: '/soy-inversionista/unidades-nuevas',
+  //       destination: '/soy-inversionista/unidades-nuevas#estacionamientos',
+  //     },
+  //   ];
+  // },
 };
 
 module.exports = nextConfig;
