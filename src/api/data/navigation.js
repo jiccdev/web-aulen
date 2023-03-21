80360aa061b7843f94818344e4e6af6690c6e8fa
Jose Contreras <ji.ccdev@outlook.com>
2023-03-21 13:24:15 -0300

    issue: change index path in unidades-nuevas

--- src/api/data/navigation.js
+++ src/api/data/navigation.js
@@ -39,7 +39,7 @@ export const navigationData = [
       {
         id: 2,
         name: 'Unidades nuevas',
-        url: '/soy-inversionista/unidades-nuevas',
+        url: '/soy-inversionista',
       },
     ],
   },
