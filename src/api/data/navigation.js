95ec05ca8ebad826f9c0772218363da586cf40ae
Jose Contreras <ji.ccdev@outlook.com>
2023-03-22 11:22:22 -0300

    feat: change links data

--- src/api/data/navigation.js
+++ src/api/data/navigation.js
@@ -7,17 +7,16 @@ export const navigationData = [
   {
     id: 2,
     name: 'Soy Propietario',
-    url: '/soy-propietario',
     submenu: [
       {
         id: 1,
         name: 'Quiero Vender',
-        url: '/soy-propietario/quiero-vender',
+        url: '/quiero-vender',
       },
       {
         id: 2,
         name: 'Quiero Arrendar',
-        url: '/soy-propietario/quiero-arrendar',
+        url: '/quiero-arrendar',
       },
     ],
   },
