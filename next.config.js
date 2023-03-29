e49764472248e581fa6df052d0794a00ead594d4
Jose Contreras <ji.ccdev@outlook.com>
2023-03-29 14:08:55 -0300

    feat: configure nextjs server file

--- next.config.js
+++ next.config.js
@@ -15,6 +15,28 @@ const nextConfig = {
       },
     ];
   },
+  headers() {
+    return [
+      {
+        source: '/:path*',
+        headers: [
+          {
+            key: 'Access-Control-Allow-Origin',
+            value: '*',
+          },
+        ],
+      },
+    ];
+  },
+  async rewrites() {
+    return [
+      {
+        source: '/:path*',
+        // destination: 'https://accionpanal.com/:path*',
+        destination: 'https://aulenpropiedades.cl/:path*',
+      },
+    ];
+  },
 };
 
 module.exports = nextConfig;
